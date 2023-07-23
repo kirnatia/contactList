62cc288240b21ac10fb419ae0f2cc959d8c2400e
kirnatia <natiakiria@gmail.com>
2023-07-23 02:04:23 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      
+      <div className="card">
+        
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
